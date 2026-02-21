d64d8fee2892802624df91dac7f713c705beb5a3
marchvps <marchvps2020@gmail.com>
2026-02-21 06:42:17 +0000

    sdm660-common: Stop forcing _vendor suffix on com.dsi.ant@1.0
    We are using the source-built HIDL interface from external/ant-wireless,
    which does not have the _vendor suffix. This fix allows blobs that depend
    on it (like the BT stack) to link correctly.

--- extract-files.py
+++ extract-files.py
@@ -35,7 +35,6 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
-        'com.dsi.ant@1.0',
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.hardware.fm@1.0',
     ): lib_fixup_vendor_suffix,
